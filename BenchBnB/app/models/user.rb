2ffa159d82e75d17ff065a6b0912270a10d05bac
kat <sarah.kathryn.peters@gmail.com>
2018-12-07 20:08:39 -0800

    Complete User model

--- BenchBnB/app/models/user.rb
+++ BenchBnB/app/models/user.rb
@@ -4,4 +4,28 @@ class User < ApplicationRecord
 
     attr_reader :password
     before_validation :ensure_session_token
+
+    def self.generate_session_token 
+        SecureRandom.urlsafe_base64(16)
+    end
+    
+    def ensure_session_token
+        self.session_token ||= self.generate_session_token
+    end
+
+    def reset_session_token 
+        self.session_token = self.generate_session_token
+        self.save
+        self.session_token
+    end
+
+    def self.find_by_credentials(username, password) 
+        user = User.find_by(username: username)
+
+        if user && valid_password?(password) 
+            return user
+        else 
+            return nil
+        end
+    end
 end
