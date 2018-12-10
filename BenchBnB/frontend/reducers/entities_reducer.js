679b66b4b3edaede3e435bc07846f13ace0fed64
kat <sarah.kathryn.peters@gmail.com>
2018-12-10 12:17:01 -0800

    Complete users and entities reducer

---
+++ BenchBnB/frontend/reducers/entities_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducer } from 'redux';
+import usersReducer from './users_reducer';
+
+const entitiesReducer = combineReducer({
+    users: usersReducer,
+})
+
+export default entitiesReducer;
---
+++ BenchBnB/frontend/reducers/users_reducer.js
@@ -0,0 +1,9 @@
+import {
+    RECEIVE_CURRENT_USER,
+    LOGOUT_CURRENT_USER,
+    RECEIVE_SESSION_ERRORS
+} from '../actions/sessions_actions';
+
+const usersReducer = () => {
+    
+}
