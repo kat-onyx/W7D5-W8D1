7b9875d252b1d7f8430ffcfeca9829993963de07
kat <sarah.kathryn.peters@gmail.com>
2018-12-10 09:55:41 -0800

    Complete sessions controller

--- BenchBnB/app/controllers/api/sessions_controller.rb
+++ BenchBnB/app/controllers/api/sessions_controller.rb
@@ -1,3 +1,22 @@
 class API::SessionsController < ApplicationController {
     
+    def create
+        user = User.find_by_credentials(username: params[:user][:username], password: params[:user][:password])
+
+        if user
+            login!(user)
+            redirect_to "api/users/show"
+        else
+            render json: { ["Invalid login credentials"]}
+        end
+
+    end
+
+    def destroy
+        user = current_user
+        logout
+    end
+
+
+end
 }
